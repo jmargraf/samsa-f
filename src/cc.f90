ef8a2961adf688f0353490887c26885de794c45d
jmargraf <jmargraf@chem.ufl.edu>
2018-02-04 22:50:20 +0100

    full rhf-ccd works / uhf-ccd + paralellization done but not testes

--- src/cc.f90
+++ src/cc.f90
@@ -20,6 +20,7 @@ subroutine calc_Elccd
   use module_ints,      only : Index2e
   use module_wavefun,   only : Fock_to_MO,Fock_to_AO
   implicit none
+  logical                   :: CCD = .true.
   integer                   :: iSpin,MOZero,iT2
   integer                   :: p3,p4,h1,h2,h5,h6,p5,p6
   integer                   :: i,j,a,b 
@@ -33,7 +34,7 @@ subroutine calc_Elccd
   double precision          :: Ecctol = 1.0d-7
   double precision          :: Eold   = 0.0d0
   double precision          :: deltaE = 99.0d0
-  double precision          :: in1
+  double precision          :: in1,in2
 
   Elccd  = 0.0d0
 
@@ -60,10 +61,14 @@ subroutine calc_Elccd
     Dijab = 1.0d0
     Elccd = 0.0d0
 
+
   do iT2=1,maxT ! T amplitude loop
     Eold  = Elccd
     t2old  = t2 !R0.5d0*(t2+t2old) ! uses mixing
+
     !T2 equations
+!$OMP PARALLEL PRIVATE(i,j,a,b,k,l,c,d,Dijab,in1)
+!$OMP DO
     do i=MOZero,nOcc
       do j=MOZero,nOcc 
         do a=nOcc+1,nmo
@@ -120,6 +125,33 @@ subroutine calc_Elccd
               enddo
             enddo
 
+            if(CCD)then ! do full CCD
+            !Terms that are quadratic in T2
+              do c = nOcc+1,nmo
+                do d = nOcc+1,nmo
+                  do k = MOZero,nOcc
+                    do l = MOZero,nOcc
+
+                      in1 = in1 + 0.5d0 *  t2old(a,c,i,k)*t2old(d,b,l,j)   * AMO(k,l,c,d)
+                      in1 = in1 - 0.5d0 *  t2old(b,c,i,k)*t2old(d,a,l,j)   * AMO(k,l,c,d)
+                      in1 = in1 + 0.5d0 *  t2old(b,c,j,k)*t2old(d,a,l,i)   * AMO(k,l,c,d)
+                      in1 = in1 - 0.5d0 *  t2old(a,c,j,k)*t2old(d,b,l,i)   * AMO(k,l,c,d)
+
+                      in1 = in1 + 0.25d0*  t2old(c,d,i,j)*t2old(a,b,k,l)   * AMO(k,l,c,d)
+
+                      in1 = in1 - 0.5d0 *  t2old(a,c,i,j)*t2old(b,d,k,l)   * AMO(k,l,c,d)
+                      in1 = in1 + 0.5d0 *  t2old(b,c,i,j)*t2old(a,d,k,l)   * AMO(k,l,c,d)
+
+                      in1 = in1 - 0.5d0 *  t2old(a,b,i,k)*t2old(c,d,j,l)   * AMO(k,l,c,d)
+                      in1 = in1 + 0.5d0 *  t2old(a,b,j,k)*t2old(c,d,i,l)   * AMO(k,l,c,d)
+
+                    enddo
+                  enddo
+                enddo
+              enddo
+            endif
+
+
 !            write(*,*) "t2   ",p3,p4,h1,h2,AMO(p3,p4,h1,h2),AMO(h1,h2,p3,p4)
 !            Dijab = (Eps_SO(p3) + Eps_SO(p4) - Eps_SO(h1) - Eps_SO(h2))
             t2(a,b,i,j) =  1.0d0*in1/Dijab
@@ -128,11 +160,10 @@ subroutine calc_Elccd
         enddo
       enddo
     enddo
+!$OMP END DO
+!$OMP END PARALLEL
 
     Elccd = 0.0d0
-!    E_2 = 0.0d0
-!    E_2OS = 0.0d0
-!    E_2SS = 0.0d0
 
     do i=MOZero,nOcc
       do j=MOZero,i-1
@@ -151,8 +182,11 @@ subroutine calc_Elccd
       write(*,*) '    '
       exit
     endif
-    write(*,*) "      LCCD: ",iT2,Elccd,deltaE 
- 
+    if(CCD)then
+      write(*,*) "      CCD: ",iT2,Elccd,deltaE
+    else
+      write(*,*) "      LCCD: ",iT2,Elccd,deltaE 
+    endif
   enddo ! t2 loop
 
   elseif(spins == 2)then
@@ -191,6 +225,8 @@ subroutine calc_Elccd
 
     !T2 equations
     !Nocc version
+!$OMP PARALLEL PRIVATE(i,j,a,b,k,l,c,d,Dijab,in1)
+!$OMP DO
     do i=MOZero,nmo
       do j=MOZero,nmo
         do a=MOZero,nmo
@@ -239,12 +275,45 @@ subroutine calc_Elccd
               enddo
             enddo
 
+            if(CCD)then ! do full CCD
+            !Terms that are quadratic in T2
+              do c = MOZero,nmo
+                do d = MOZero,nmo
+                  do k = MOZero,nmo
+                    do l = MOZero,nmo
+                      if((SO_Occ(k) == 0.0) .or. (SO_Occ(l) == 0.0))then
+                        cycle
+                      elseif((SO_Occ(c) == 1.0) .or. (SO_Occ(d) == 1.0))then
+                        cycle
+                      endif
+
+                      in1 = in1 + 0.5d0 *  t2old(a,c,i,k)*t2old(d,b,l,j)   * AMO(k,l,c,d)
+                      in1 = in1 - 0.5d0 *  t2old(b,c,i,k)*t2old(d,a,l,j)   * AMO(k,l,c,d)
+                      in1 = in1 + 0.5d0 *  t2old(b,c,j,k)*t2old(d,a,l,i)   * AMO(k,l,c,d)
+                      in1 = in1 - 0.5d0 *  t2old(a,c,j,k)*t2old(d,b,l,i)   * AMO(k,l,c,d)
+
+                      in1 = in1 + 0.25d0*  t2old(c,d,i,j)*t2old(a,b,k,l)   * AMO(k,l,c,d)
+
+                      in1 = in1 - 0.5d0 *  t2old(a,c,i,j)*t2old(b,d,k,l)   * AMO(k,l,c,d)
+                      in1 = in1 + 0.5d0 *  t2old(b,c,i,j)*t2old(a,d,k,l)   * AMO(k,l,c,d)
+
+                      in1 = in1 - 0.5d0 *  t2old(a,b,i,k)*t2old(c,d,j,l)   * AMO(k,l,c,d)
+                      in1 = in1 + 0.5d0 *  t2old(a,b,j,k)*t2old(c,d,i,l)   * AMO(k,l,c,d)
+
+                    enddo
+                  enddo
+                enddo
+              enddo
+            endif
+
             t2(a,b,i,j) =  1.0d0*in1/Dijab
 
           enddo
         enddo
       enddo
     enddo
+!$OMP END DO
+!$OMP END PARALLEL
 
     Elccd = 0.0d0
 !    E_2 = 0.0d0
